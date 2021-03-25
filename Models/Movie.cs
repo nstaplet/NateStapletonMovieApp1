3903bdaf9038c645c336d0d184b3e031fcdb13a5
nstaplet <nstaplet7@gmail.com>
2021-03-24 21:37:29 -0600

    Assignment 9

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace NateStapletonMovieApp.Models
 {
     public class Movie
     {
+        [Key]
+        public int MovieId { get; set; }
         public string MovieTitle { get; set; }
         public string Catagory { get; set; }
         public int Year { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NateStapletonMovieApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,13 +20,23 @@ namespace NateStapletonMovieApp
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MovieDbContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:MovieDatabaseConnection"]);
+            });
+
+            services.AddScoped<IMovieRepository, EFMovieRepository>();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
